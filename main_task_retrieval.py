94a0e6201a4b61a30b03a22b62787c85abd220d3
Milabo-HRH <Ruihuanh3@zju.edu.cn>
2023-05-06 20:26:23 +0800

    debug preemption

--- main_task_retrieval.py
+++ main_task_retrieval.py
@@ -581,15 +581,15 @@ def main():
                 # exit(1)
             NoMem = True
             while(NoMem):
-            try:
-                train_sampler.set_epoch(epoch)
-                tr_loss, global_step = train_epoch(epoch, args, model, train_dataloader, device, n_gpu, optimizer,
+                try:
+                    train_sampler.set_epoch(epoch)
+                    tr_loss, global_step = train_epoch(epoch, args, model, train_dataloader, device, n_gpu, optimizer,
                                                scheduler, global_step, local_rank=args.local_rank)
-            except:
-                logger.info("Out of memory, retrying...")
-                sleep(100)
-            else:
-                NoMem = False
+                except:
+                    logger.info("Out of memory, retrying...")
+                    sleep(100)
+                else:
+                    NoMem = False
             if args.local_rank == 0:
                 logger.info("Epoch %d/%s Finished, Train Loss: %f", epoch + 1, args.epochs, tr_loss)
                 if(epoch%args.save_epoch==0):
