22637eb268fec57ed820330eda8a53c9f7752fb5
Milabo-HRH <Ruihuanh3@zju.edu.cn>
2023-05-14 14:42:47 +0800

    try split the dataset

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import torch
 import torchvision
-from modules.until_module import align_state_dicts
-state_dict = torch.load('./ckpts/ckpt_wechat_retrieval_looseType/pytorch_model.bin.0', map_location="cpu")
-for k in list(state_dict.keys()):
-    print(k)
-# align_state_dicts(state_dict)
-# lz = torch.jit.load('./modules/ViT-B-16.pt', map_location=torch.device('cpu'))
-# for parameter in lz.state_dict():
-#     if parameter not in state_dict.keys():
-#         print("\""+parameter+"\",")
-# print((lz.state_dict()['transformer.resblocks.3.attn.in_proj_weight']).shape)
-# print((state_dict['module.bert.encoder.layer.3.attention.self.query.weight']).shape)
-# print(torchvision.__version__)
-# print(lz.state_dict()['positional_embedding'].shape)
-# print(state_dict['module.bert.embeddings.position_embeddings.weight'])
+from sklearn.model_selection import train_test_split
+import json
+
+with open('data/annotations/labeled.json', 'r') as f:
+    data = json.load(f)
+
+train, test = train_test_split(data, test_size=0.1, shuffle=True, stratify=data['label'])
+
+with open('data/annotations/train.json', 'w') as f:
+    json.dump(train, f)
+
+# save test data to json file
+with open('data/annotations/test.json', 'w') as f:
+    json.dump(test, f)
